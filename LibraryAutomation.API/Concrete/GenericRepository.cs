8eca01fb269e9c232a7ed08005b29f412309d86f
aksoyerol <erolaksoy98@gmail.com>
2021-05-04 00:00:52 +0300

    Add GenericRepository

---
+++ LibraryAutomation.API/Concrete/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using LibraryAutomation.API.Context;
+using LibraryAutomation.API.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LibraryAutomation.API.Concrete
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        private AppDbContext _context;
+        private DbSet<TEntity> _dbSet;
+
+        public GenericRepository(AppDbContext appDbContext)
+        {
+            _context = appDbContext;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(TEntity entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public TEntity Update(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            _context.SaveChanges();
+            return entity;
+        }
+
+        public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+        {
+            return _dbSet.Where(predicate);
+        }
+    }
+}
